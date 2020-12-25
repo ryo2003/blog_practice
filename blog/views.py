f0a849155df4bddfe01db4b441cb463e26131f36
Your Name <you@example.com>
2020-12-25 06:44:25 +0000

    crud

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# ListViewとDetailViewを取り込み
+from django.views.generic import ListView, DetailView
+
+# ListViewは一覧を簡単に作るためのView
+class Index(ListView):
+    # 一覧するモデルを指定 -> `object_list`で取得可能
+    model = Post
+
+# DetailViewは詳細を簡単に作るためのView
+class Detail(DetailView):
+    # 詳細表示するモデルを指定 -> `object`で取得可能
+    model = Post 
