a43d85cef9537bb80c76f6d67a7ea11d80608c02
Your Name <you@example.com>
2020-12-25 04:20:58 +0000

    models prac

--- pj_blog/settings.py
+++ pj_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'a^9j-ocwp7-^-mx24icfi_#qqokc$m-0$0cf06qwjz85#ufpf!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
